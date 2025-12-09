60b7529e0425c57cee07c2c298831bb69cdcb10b
Tracy Bachmann <tracy.bachmann68@gmail.com>
2025-12-09 16:22:22 +0100

    Ajout de ma salutation au projet

--- src/Salutation.java
+++ src/Salutation.java
@@ -1,12 +1,9 @@
 public class Salutation { 
 public static void main(String[] args) { 
         System.out.println("Bonjour de Amos Dorceus"); 
-<<<<<<< Updated upstream
         System.out.println("Hello, user!"); 
-=======
         System.out.println("Hello, user!");
         System.out.println("Bonjour de Tracy Bachmann !");
         System.out.println("Hello from Diginamic !");
->>>>>>> Stashed changes
     } 
 } 
