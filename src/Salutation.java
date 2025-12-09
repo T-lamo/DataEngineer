459c7f54a64037e53dbc00beb5953d1d0f1eafac
Amos Dorceus <amosdorceus2010@gmail.com>
2025-12-09 21:50:44 +0100

    ssh set up

--- src/Salutation.java
+++ src/Salutation.java
@@ -1,6 +1,12 @@
 public class Salutation { 
 public static void main(String[] args) { 
         System.out.println("Bonjour de Amos Dorceus"); 
+<<<<<<< Updated upstream
         System.out.println("Hello, user!"); 
+=======
+        System.out.println("Hello, user!");
+        System.out.println("Bonjour de Tracy Bachmann !");
+        System.out.println("Hello from Diginamic !");
+>>>>>>> Stashed changes
     } 
 } 
